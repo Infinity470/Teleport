da4b498b33fdcd3c265dab797c93f4212c2d9290
OVERLOAD48 <166378180+OVERLOAD470@users.noreply.github.com>
2024-04-15 20:22:52 +0600

    Update Teleport.java

--- src/main/java/com/arctic/Teleport.java
+++ src/main/java/com/arctic/Teleport.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -26,87 +27,66 @@ import org.bukkit.plugin.java.JavaPlugin;
 
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.RED + "Only players can use this command.");
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (cmd.getName().equalsIgnoreCase("tp")) {
+
+                    if (args.length != 1) {
+                        player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.RED + " Usage: /tp <player>");
+                        return true;
+                    }
+
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target == null) {
+                        player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.RED + " Player not found or not online.");
+                        return true;
+                    }
+
+                    player.teleport(target.getLocation());
+                    player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.GREEN + " Teleported to " + target.getName());
+                    return true;
+                }
+
+                if (cmd.getName().equalsIgnoreCase("tphere")) {
+
+                    if (args.length != 1) {
+                        player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.RED + " Usage: /tphere <player>");
+                        return true;
+                    }
+
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target == null) {
+                        player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.RED + " Player not found or not online.");
+                        return true;
+                    }
+
+                    target.teleport(player.getLocation());
+                    player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.GREEN + " Teleported " + target.getName() + " to you.");
+                    return true;
+                }
+
+                if (cmd.getName().equalsIgnoreCase("vanish")) {
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        onlinePlayer.hidePlayer(player);
+                    }
+                    player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.GREEN + " You are now vanished.");
+
+                    return true;
+                }
+
+                if (cmd.getName().equalsIgnoreCase("unvanish")) {
+
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        onlinePlayer.showPlayer(player);
+                    }
+                    player.sendMessage(ChatColor.AQUA + "[TP]" + ChatColor.GREEN + " You are now visible to others.");
+                    return true;
+                }
+            } else if (sender instanceof ConsoleCommandSender) {
+                sender.sendMessage(ChatColor.RED + "Only players can use this command!");
                 return true;
             }
-
-            Player player = (Player) sender;
-
-            if (cmd.getName().equalsIgnoreCase("tp")) {
-                if (args.length != 1) {
-                    player.sendMessage("Usage: /tp <player>");
-                    return true;
-                }
-
-                if (!player.hasPermission("teleport.tp")) {
-                    player.sendMessage("You do not have permission to use this command.");
-                    return true;
-                }
-
-
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target == null) {
-                    player.sendMessage(ChatColor.RED + "Player not found or not online.");
-                    return true;
-                }
-
-                player.teleport(target.getLocation());
-                player.sendMessage(ChatColor.GREEN + "Teleported to " + target.getName());
-                return true;
-            }
-
-            if (cmd.getName().equalsIgnoreCase("tphere")) {
-                if (args.length != 1) {
-                    player.sendMessage("Usage: /tphere <player>");
-                    return true;
-                }
-
-                if (!player.hasPermission("teleport.tphere")) {
-                    player.sendMessage("You do not have permission to use this command.");
-                    return true;
-                }
-
-                Player target = Bukkit.getPlayer(args[0]);
-                if (target == null) {
-                    player.sendMessage(ChatColor.RED + "Player not found or not online.");
-                    return true;
-                }
-
-                target.teleport(player.getLocation());
-                player.sendMessage(ChatColor.GREEN + "Teleported " + target.getName() + " to you.");
-                return true;
-            }
-
-            if (cmd.getName().equalsIgnoreCase("vanish")) {
-                if (!player.hasPermission("teleport.vanish")) {
-                    player.sendMessage("You do not have permission to use this command.");
-                    return true;
-                }
-
-                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                    onlinePlayer.hidePlayer(player);
-                }
-                player.sendMessage(ChatColor.GREEN + "You are now vanished.");
-
-                return true;
-            }
-
-            if (cmd.getName().equalsIgnoreCase("unvanish")) {
-                if (!player.hasPermission("teleport.unvanish")) {
-                    player.sendMessage("You do not have permission to use this command.");
-                    return true;
-                }
-
-                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                    onlinePlayer.showPlayer(player);
-                }
-                player.sendMessage(ChatColor.GREEN + "You are now visible to others.");
-                return true;
-            }
-
             return false;
         }
     }
-
-
